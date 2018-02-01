be79390e839410fbb1c67553fa54d28e79fd27cd
matthew.molnar <matthew.molnar@gmail.com>
2018-02-01 17:38:04 -0500

    Init

---
+++ app.js
@@ -0,0 +1,47 @@
+// class Blockchain(object):
+//     def __init__(self):
+//         self.chain = []
+//         self.current_transactions = []
+        
+//     def new_block(self):
+//         # Creates a new Block and adds it to the chain
+//         pass
+    
+//     def new_transaction(self):
+//         # Adds a new transaction to the list of transactions
+//         pass
+    
+//     @staticmethod
+//     def hash(block):
+//         # Hashes a Block
+//         pass
+
+//     @property
+//     def last_block(self):
+//         # Returns the last Block in the chain
+//         pass
+
+        
+class Blockchain {
+  constructor() {
+    super();
+    this.chain = [];
+    this.current_transactions = [];
+  }
+  
+  new_block() {
+    
+  }
+  
+  new_transaction() {
+    
+  }
+  
+  static hash() {
+    
+  }
+  
+  get last_block() {
+    
+  }
+}
